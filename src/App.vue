6b0b39062ce32db828e29a62f3856d50763e7829
SamHacker <xux510208@gmail.com>
2026-04-26 09:42:39 +0800

    💄 chore(ui): 移除不必要的代碼並簡化主界面結構

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/core";
-
-const greetMsg = ref("");
-const name = ref("");
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsg.value = await invoke("greet", { name: name.value });
-}
 </script>
 
 <template>
-  <main></main>
+  <main>
+    <TitleBar />
+  </main>
 </template>
