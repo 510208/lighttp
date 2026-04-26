056781ac00b67837da5b1d3d14e2557dd11e2c92
SamHacker <xux510208@gmail.com>
2026-04-26 14:22:22 +0800

    ✨ feat(ParamTable): 調整參數表格顯示邏輯以改善滾動效果 💄 style: 更新邊框顏色以符合 Catppuccin 主題

--- src/components/SettingPanel/params/ParamTable.vue
+++ src/components/SettingPanel/params/ParamTable.vue
@@ -9,27 +9,29 @@
         <Plus />新增參數
       </Button>
     </div>
-    <Table class="w-full table-fixed">
-      <TableHeader>
-        <TableRow class="hover:bg-transparent border-ctp-surface2">
-          <TableHead class="w-12 text-center">
-            <Check class="mx-auto text-ctp-subtext1" :size="16" />
-          </TableHead>
-          <TableHead class="w-1/2">參數</TableHead>
-          <TableHead class="w-1/2">值</TableHead>
-          <TableHead class="w-1/2" />
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <ParamTableLine
-          v-for="item in requestStore.params"
-          :key="item.id"
-          :enabled="true"
-          :param="item.key"
-          :value="item.value"
-        />
-      </TableBody>
-    </Table>
+    <div class="max-h-60 overflow-x-hidden overflow-y-auto border rounded-lg">
+      <Table class="w-full table-fixed">
+        <TableHeader>
+          <TableRow class="hover:bg-transparent border-ctp-surface2">
+            <TableHead class="w-12 text-center">
+              <Check class="mx-auto text-ctp-subtext1" :size="16" />
+            </TableHead>
+            <TableHead class="w-1/2">參數</TableHead>
+            <TableHead class="w-1/2">值</TableHead>
+            <TableHead class="w-1/2" />
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <ParamTableLine
+            v-for="item in requestStore.params"
+            :key="item.id"
+            :enabled="true"
+            :param="item.key"
+            :value="item.value"
+          />
+        </TableBody>
+      </Table>
+    </div>
   </div>
 </template>
 
