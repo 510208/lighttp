4b77bd77dadf8e282b29541e59a452e5cf28df8c
SamHacker <xux510208@gmail.com>
2026-04-26 10:59:36 +0800

    ✨ feat(TargetInput): 新增 TargetInput 組件

---
+++ src/layouts/RequestBuilder.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col px-10 py-7.5">
+    <!-- 請求輸入框 -->
+    <TargetInput />
+    <!-- 設定面板 -->
+  </div>
+</template>
+
+<script setup lang="ts">
+import TargetInput from "@/components/TargetInput.vue";
+</script>
+
+<style scoped></style>
