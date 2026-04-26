cb6f756ed5b24a3452aeeab2e5fd8373c26770ba
SamHacker <xux510208@gmail.com>
2026-04-26 10:33:38 +0800

    ✨ feat(App): 新增 RequestBuilder 組件至主應用程式

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TitleBar from "./components/core/TitleBar.vue";
+import RequestBuilder from "./components/sections/RequestBuilder.vue";
 </script>
 
 <template>
   <main>
     <TitleBar />
+    <RequestBuilder />
   </main>
 </template>
