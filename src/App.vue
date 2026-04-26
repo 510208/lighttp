219413d1ce6fc47234b5c6270ed7db16160b67b7
SamHacker <xux510208@gmail.com>
2026-04-26 09:43:14 +0800

    ✨ feat(App): 新增問候訊息和名稱的狀態管理

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/core";
+import TitleBar from "./components/core/TitleBar.vue";
+
+const greetMsg = ref("");
+const name = ref("");
 </script>
 
 <template>
