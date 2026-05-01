a1bc384138b71e4ea48ab48588a5a0bb837545e1
SamHacker <xux510208@gmail.com>
2026-05-01 22:51:51 +0800

    ✨ feat(auth): 新增 Bearer Token 認證方式

--- src/constants/methods.ts
+++ src/constants/methods.ts
@@ -1,5 +1,6 @@
 import AuthNone from "@/components/SettingPanel/auth/method/AuthNone.vue";
 import AuthBasic from "@/components/SettingPanel/auth/method/AuthBasic.vue";
+import AuthBearer from "@/components/SettingPanel/auth/method/AuthBearer.vue";
 
 export const authMethods = [
   {
@@ -14,6 +15,13 @@ export const authMethods = [
       "基本驗證方式。\n\n使用此方式後系統會自動在 Header 中加入 Authorization: Basic ... 標頭。",
     object: AuthBasic,
     label: "Basic",
+  },
+  {
+    value: "Bearer",
+    description:
+      "Bearer Token 認證方式。\n\nBearer Token 是一種常見的 API 認證方式，通常用於 OAuth 2.0 中。\n使用此方式後系統會自動在 Header 中加入 Authorization: Bearer ... 標頭。",
+    object: AuthBearer,
+    label: "Bearer Token",
   },
 ];
 
