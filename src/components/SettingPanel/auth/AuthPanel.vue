c16bcf9f4c72e30d69b6e974ec248e827630ddff
SamHacker <xux510208@gmail.com>
2026-05-01 21:15:53 +0800

    ✨ feat(AuthPanel): 新增可調整大小的認證面板組件

--- src/components/SettingPanel/auth/AuthPanel.vue
+++ src/components/SettingPanel/auth/AuthPanel.vue
@@ -1,7 +1,32 @@
 <template>
-  <div class="flex"></div>
+  <ResizablePanelGroup
+    direction="horizontal"
+    class="border rounded-lg border-ctp-surface1"
+  >
+    <!-- 左欄：顯示認證選項 -->
+    <ResizablePanel :default-size="30">
+      <div class="flex h-[200px] items-center justify-center p-6">
+        <span class="font-semibold">One</span>
+      </div>
+    </ResizablePanel>
+
+    <ResizableHandle />
+
+    <!-- 右欄：設定詳細內容 -->
+    <ResizablePanel :default-size="70">
+      <div class="flex h-[200px] items-center justify-center p-6">
+        <span class="font-semibold">One</span>
+      </div>
+    </ResizablePanel>
+  </ResizablePanelGroup>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+</script>
 
 <style scoped></style>
