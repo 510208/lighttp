58f0af1044c1bf017f10ac5d4c7e468736c65021
SamHacker <xux510208@gmail.com>
2026-05-01 23:38:06 +0800

    ✨ feat(TargetInput): 新增發送請求的邏輯並改善日誌輸出

--- src/components/TargetInput/TargetInput.vue
+++ src/components/TargetInput/TargetInput.vue
@@ -8,31 +8,45 @@ import { Button } from "@/components/ui/button";
 
 const requestStore = useRequestStore();
 
+import { invoke } from "@tauri-apps/api/core";
+
 // 處理發送邏輯 (未來對接 Rust 後端)
 const handleSend = () => {
   if (requestStore.method === "NONE") {
     console.warn("未選擇 HTTP 方法，跳過請求");
     return;
   }
+
   console.log(`Sending ${requestStore.method} request to: ${requestStore.url}`);
+  console.log("requestStore:", requestStore);
+  console.log("Request Content:", requestStore.getRequestData());
+
+  // 發送請求到 Rust 後端
+  invoke("handle_request", { payload: requestStore.getRequestData() })
+    .then((response) => {
+      console.log("Response from Rust:", response);
+    })
+    .catch((error) => {
+      console.error("Error invoking Rust command:", error);
+    });
 };
 </script>
 
 <template>
   <div
-    class="flex flex-1 items-stretch bg-ctp-base rounded-lg border border-ctp-surface0 focus-within:border-ctp-surface2 transition-colors duration-200 overflow-hidden"
+    class="bg-ctp-base border-ctp-surface0 focus-within:border-ctp-surface2 flex flex-1 items-stretch overflow-hidden rounded-lg border transition-colors duration-200"
   >
     <MethodSelect />
     <input
       v-model="requestStore.url"
       type="text"
       placeholder="Enter URL or paste text"
-      class="bg-transparent outline-none flex-1 px-4 py-2 text-sm text-[#cdd6f4] placeholder-[#6c7086]"
+      class="flex-1 bg-transparent px-4 py-2 text-sm text-[#cdd6f4] placeholder-[#6c7086] outline-none"
       @keyup.enter="handleSend"
     />
     <Button
       @click="handleSend"
-      class="font-bold px-6 py-2 rounded-none transition-all text-ctp-subtext0 text-base cursor-pointer min-h-12.5 w-24 bg-ctp-surface0 hover:bg-ctp-surface2"
+      class="text-ctp-subtext0 bg-ctp-surface0 hover:bg-ctp-surface2 min-h-12.5 w-24 cursor-pointer rounded-none px-6 py-2 text-base font-bold transition-all"
     >
       送出
       <SendHorizontal />
