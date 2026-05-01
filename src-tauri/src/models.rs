8a9368c12123f8809069e23910b2094ac8bd0115
SamHacker <xux510208@gmail.com>
2026-05-01 23:38:44 +0800

    ✨ feat(models): 新增資料結構以處理 HTTP 請求的標頭、參數和認證

---
+++ src-tauri/src/models.rs
@@ -0,0 +1,42 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Debug)]
+pub struct Header {
+    pub key: String,
+    pub value: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Param {
+    pub key: String,
+    pub value: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct AuthStore {
+    pub auth_type: String,
+    pub username: Option<String>,
+    pub password: Option<String>,
+    pub token: Option<String>,
+}
+
+// 定義前端資料結構
+
+// #[derive(Deserialize)]代表這個結構可以從 JSON 反序列化而來，這對於從前端接收資料非常有用。
+// {      url: url.value,
+//       method: method.value,
+//       params: params.value.filter((p) => p.enabled),
+//       headers: headers.value.filter((h) => h.enabled),
+//       auth: auth.value,}
+
+#[derive(Deserialize, Debug)]
+pub struct RequestPayload {
+    pub url: String,
+    pub method: String,
+    pub params: Vec<Param>,
+    pub headers: Vec<Header>,
+    pub auth: AuthStore,
+}
+
+// ------
+
