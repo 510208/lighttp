2881a9a9587c057333f3d8f2bce550f20be3f680
SamHacker <xux510208@gmail.com>
2026-05-02 23:01:37 +0800

    ✨ feat(BodyChoose): 新增 BodyChoose 組件以選擇認證方式

---
+++ src/components/SettingPanel/body/BodyChoose.vue
@@ -0,0 +1,36 @@
+<template>
+  <Select @update:model-value="(v) => emit('update:modelValue', v)">
+    <SelectTrigger size="sm">
+      <SelectValue placeholder="選擇認證方式" />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectItem
+        v-for="method in bodyMethods"
+        :key="method.value"
+        :value="method.value"
+      >
+        {{ method.label }}
+      </SelectItem>
+    </SelectContent>
+  </Select>
+</template>
+
+<script setup lang="ts">
+import { bodyMethods } from "@/constants/methods";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+// 接收父組件傳入的 v-model
+defineProps<{
+  modelValue?: string;
+}>();
+
+const emit = defineEmits(["update:modelValue"]);
+</script>
+
+<style scoped></style>
