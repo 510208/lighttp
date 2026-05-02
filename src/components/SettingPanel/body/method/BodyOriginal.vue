2608d7812489852c961d052c35b73b54817132c3
SamHacker <xux510208@gmail.com>
2026-05-02 23:06:58 +0800

    ✨ feat(BodyOriginal): 新增 BodyOriginal 組件以顯示代碼編輯器

---
+++ src/components/SettingPanel/body/method/BodyOriginal.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-ui flex w-full flex-col gap-4"><CodeEditor /></div>
+</template>
+
+<script setup lang="ts">
+import CodeEditor from "@/components/ui/editor/CodeEditor.vue";
+</script>
