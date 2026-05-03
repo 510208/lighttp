7b1e11a64b572c9157ff153f6936a03900f18fa9
SamHacker <xux510208@gmail.com>
2026-05-03 20:19:52 +0800

    ✨ feat(proxy): 更新代理協定以支援 socks4

--- src/stores/proxyConfig.d.ts
+++ src/stores/proxyConfig.d.ts
@@ -1,6 +1,6 @@
 export interface ProxyConfig {
   enabled: boolean;
-  protocol: "http" | "https" | "socks5"; // e.g., "http", "https", "socks5"
+  protocol: "http" | "https" | "socks4" | "socks5"; // e.g., "http", "https", "socks4", "socks5"
   host: string;
   port: number;
   auth?: {
