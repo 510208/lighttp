ff9b992da251d5fb825618c13272c177f683614e
SamHacker <xux510208@gmail.com>
2026-05-04 19:41:30 +0800

    💄 feat(ResponsePanel): 調整回應面板的樣式與標題顯示

--- src/components/ResponsePanel/ResponsePanel.vue
+++ src/components/ResponsePanel/ResponsePanel.vue
@@ -1,10 +1,13 @@
 <template>
   <!-- 回應面板 -->
   <div
-    class="bg-ctp-surface border-ctp-surface-variant flex h-full min-h-0 flex-col"
+    class="bg-ctp-surface border-ctp-surface-variant flex h-full min-h-0 flex-col gap-0"
   >
+    <div class="flex items-center">
+      <h2 class="text-ctp-overlay2 mt-4 px-10 text-sm font-bold">結果</h2>
+    </div>
     <Tabs default-value="body" class="flex min-h-0 flex-1 flex-col">
-      <TabsList class="mt-4 gap-5 bg-transparent p-4 px-10">
+      <TabsList class="mt-2 gap-5 bg-transparent p-4 px-10">
         <TabsOptions value="body" name="Body" chinese-name="主體" />
         <TabsOptions value="headers" name="Headers" chinese-name="標頭" />
       </TabsList>
