9f7e2d7f1eda217c1a3d260bc192b7a49422c04a
SamHacker <xux510208@gmail.com>
2026-05-04 16:24:51 +0800

    ✨ feat(ResponsePanel): 新增回應面板以顯示 JSON 數據

---
+++ src/components/ResponsePanel/body/BodyPanel.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <CodeViewer :model-value="JSON.stringify(responseStore.body, null, 2)" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import CodeViewer from "@/components/ui/editor/CodeViewer.vue";
+import { useResponseStore } from "@/stores/useResponseStore";
+
+const responseStore = useResponseStore();
+</script>
+
+<style scoped></style>
