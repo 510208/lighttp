0acfc1e1793b631a2cd134af522a0860208490be
SamHacker <xux510208@gmail.com>
2026-05-04 21:48:31 +0800

    ✨ feat(IconContainer): 增加變體屬性以支持錯誤狀態顯示

--- src/components/ui/icon-ct/IconContainer.vue
+++ src/components/ui/icon-ct/IconContainer.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="bg-ctp-surface1 rounded-lg border p-2">
+  <div
+    class="rounded-lg border p-2"
+    :class="
+      variant === 'error'
+        ? 'bg-ctp-red-950 border-ctp-red-500'
+        : 'bg-ctp-surface0'
+    "
+  >
     <slot />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface Props {
+  variant?: "default" | "error";
+}
+
+defineProps<Props>();
+</script>
