be0a7e73b68579a9d0f4b0094bb645bad238f305
SamHacker <xux510208@gmail.com>
2026-05-04 21:27:14 +0800

    ✨ feat(StatusBadge): 增加狀態屬性以支持更多狀態顏色

--- src/components/StatusBar/StatusBadge.vue
+++ src/components/StatusBar/StatusBadge.vue
@@ -9,7 +9,15 @@
 import { cn } from "@/lib/utils";
 
 interface Props {
-  status?: "ready" | "error" | "success" | "loading" | "none";
+  status?:
+    | "ready"
+    | "error"
+    | "errorText"
+    | "success"
+    | "successText"
+    | "loading"
+    | "loadingText"
+    | "none";
   class?: string;
   icon?: any; // 可選的圖標屬性，類型為任何（通常是Lucide圖標組件）
 }
@@ -17,8 +25,11 @@ interface Props {
 const statusColors = {
   ready: "bg-ctp-overlay2 text-ctp-base",
   error: "bg-ctp-red text-ctp-base",
+  errorText: "text-ctp-red",
   success: "bg-ctp-green text-ctp-base",
+  successText: "text-ctp-green",
   loading: "bg-ctp-peach text-ctp-base",
+  loadingText: "text-ctp-peach",
   none: "hover:bg-ctp-surface0 transition-colors duration-200 text-white pointer-events-none text-ctp-subtext0",
 };
 const generalClass = "px-3 text-xs flex h-full items-center flex gap-1";
