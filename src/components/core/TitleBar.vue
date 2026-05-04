d8c87f2283d17be87594b54b6fee13dd5e431eb4
SamHacker <xux510208@gmail.com>
2026-05-04 14:52:24 +0800

    💄 ui(TitleBar): 調整 TitleBar 組件的樣式和結構

--- src/components/core/TitleBar.vue
+++ src/components/core/TitleBar.vue
@@ -1,15 +1,15 @@
 <template>
   <div
-    class="flex h-15 w-full pl-6 items-center gap-2.5"
+    class="bg-ctp-surface sticky top-0 z-10 flex h-15 w-full items-center gap-2.5 pl-6"
     data-tauri-drag-region
   >
     <div class="flex gap-2.5">
-      <img :src="logo" alt="Logo" class="h-5 pointer-events-none" />
-      <p class="text-sm text-ctp-subtext1 pointer-events-none">by SamHacker</p>
+      <img :src="logo" alt="Logo" class="pointer-events-none h-5" />
+      <p class="text-ctp-subtext1 pointer-events-none text-sm">by SamHacker</p>
     </div>
 
     <!-- 視窗控制按鈕 -->
-    <div class="absolute right-0 top-0">
+    <div class="absolute top-0 right-0">
       <WindowControlButton />
     </div>
   </div>
