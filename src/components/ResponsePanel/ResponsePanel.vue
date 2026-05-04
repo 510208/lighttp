5f16f79a155f968b484760261181dde08023f91e
SamHacker <xux510208@gmail.com>
2026-05-04 16:14:00 +0800

    ✨ feat(ResponsePanel): 更新回應面板的標籤和內容以支持主體顯示

--- src/components/ResponsePanel/ResponsePanel.vue
+++ src/components/ResponsePanel/ResponsePanel.vue
@@ -1,13 +1,13 @@
 <template>
   <!-- 回應面板 -->
   <div class="bg-ctp-surface border-ctp-surface-variant h-full min-h-0 p-4">
-    <Tabs default-value="params">
+    <Tabs default-value="body">
       <TabsList class="gap-5 bg-transparent p-0">
-        <TabsOptions value="params" name="Params" chinese-name="參數" />
+        <TabsOptions value="body" name="Body" chinese-name="主體" />
         <TabsOptions value="headers" name="Headers" chinese-name="標頭" />
       </TabsList>
       <div id="setting-panel-content" class="min-h-30">
-        <TabsContent value="params" as-child>ParamTable /></TabsContent>
+        <TabsContent value="body" as-child>ParamTable /></TabsContent>
         <TabsContent value="headers" as-child>HeaderTable /></TabsContent>
       </div>
     </Tabs>
