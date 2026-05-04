ad5fd2cf702fd39ee127269ad7cac5d06ad1dec2
SamHacker <xux510208@gmail.com>
2026-05-04 15:45:35 +0800

    ✨ feat(TabsOption): 將TabsTrigger 組件改成共用

--- src/components/common/TabsOption.vue
+++ src/components/common/TabsOption.vue
@@ -2,7 +2,7 @@
   <TabsTrigger
     :value="value"
     :disabled="disabled"
-    class="flex items-center justify-end gap-2.5 border-none px-2 py-0.5"
+    class="flex h-8 items-center justify-end gap-2.5 border-none px-2 py-0"
   >
     <p class="text-sm font-medium">{{ name || value }}</p>
     <p class="text-ctp-text/20 sh_tabs_chinese-name text-[10px] font-light">
