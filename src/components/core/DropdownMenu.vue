6123658bd8b27c12c3b842e7e39f53de6dcf34c3
SamHacker <xux510208@gmail.com>
2026-05-10 20:58:36 +0800

    ✨ feat(DropdownMenu): 新增下拉選單以顯示關於 LigHTTP 的資訊

--- src/components/core/DropdownMenu.vue
+++ src/components/core/DropdownMenu.vue
@@ -1,7 +1,33 @@
 <template>
-  <div></div>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="ghost" size="icon-sm">
+        <EllipsisVertical />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent>
+      <DropdownMenuItem @click="Window.openAboutWindow()">
+        <Info />
+        關於 LigHTTP
+      </DropdownMenuItem>
+
+      <DropdownMenuSeparator />
+    </DropdownMenuContent>
+  </DropdownMenu>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { Info, EllipsisVertical } from "@lucide/vue";
+import { Button } from "@/components/ui/button";
+
+import { Window } from "@/services";
+</script>
 
 <style scoped></style>
