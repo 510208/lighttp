c9d1a79973ab4091e200eb21a44b9357f8b138f0
SamHacker <xux510208@gmail.com>
2026-05-17 12:56:10 +0800

    🌐 i18n: 更新 setLanguage 函數以重新載入語言設定

--- src/stores/useSettingsStore.ts
+++ src/stores/useSettingsStore.ts
@@ -1,11 +1,15 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { changeLang } from "@/i18n";
 
 export const useSettingsStore = defineStore("settings", () => {
   const language = ref("zh-TW");
 
   function setLanguage(newLang: string) {
     language.value = newLang;
+
+    // 重新載入語言以應用新的語言設定
+    changeLang(newLang);
   }
 
   return {
