4b4eab392f871e79e9d1d9c23b861c6f7a3281ec
SamHacker <xux510208@gmail.com>
2026-05-17 13:35:05 +0800

    ➕ chore(dependencies): 新增 tauri-store 和 tauri-plugin-pinia 依賴，並更新相關配置

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,6 +12,8 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_store::init())
+        .plugin(tauri_plugin_pinia::init())
         .invoke_handler(tauri::generate_handler![greet, handle_request])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { i18n } from "@/i18n";
+import { TauriPluginPinia } from "@tauri-store/pinia"; // 引入外掛
 
 import App from "./App.vue";
 import "@/styles/index.css";
 import router from "@/router";
 
 const app = createApp(App);
-app.use(createPinia());
+
+const pinia = createPinia();
+pinia.use(TauriPluginPinia());
+
+app.use(pinia);
 app.use(i18n);
 app.use(router);
 app.mount("#app");
