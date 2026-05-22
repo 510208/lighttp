134fee42ba2a99ea0d8a39f1739985f9d23ba2ef
SamHacker <xux510208@gmail.com>
2026-05-22 23:36:19 +0800

    ✨ feat(settings): 新增 hexViewerConfig 主題選擇功能以支援十六進位檢視器

--- src/components/settings/pages/ThemesSettings.vue
+++ src/components/settings/pages/ThemesSettings.vue
@@ -13,6 +13,31 @@
         v-model="settingsStore.backgroundImageUrl"
       />
     </ConfigOptionFrame>
+    <Separator />
+    <ConfigOptionFrame
+      label-key="settings_panel.themes.binary_editor_theme.label"
+      description-key="settings_panel.themes.binary_editor_theme.description"
+    >
+      <Select v-model="settingsStore.hexViewerConfig.theme">
+        <SelectTrigger>
+          <SelectValue placeholder="選擇主題" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectLabel>
+            {{ $t("settings_panel.themes.binary_editor_theme.dark_themes") }}
+          </SelectLabel>
+          <SelectItem value="dark">
+            Dark ({{ $t("settings_panel.themes.binary_editor_theme.default") }})
+          </SelectItem>
+          <SelectItem value="terminal">Terminal</SelectItem>
+          <SelectItem value="sunset">Sunset</SelectItem>
+          <SelectLabel>
+            {{ $t("settings_panel.themes.binary_editor_theme.light_themes") }}
+          </SelectLabel>
+          <SelectItem value="light">Light</SelectItem>
+        </SelectContent>
+      </Select>
+    </ConfigOptionFrame>
   </div>
 </template>
 
@@ -20,6 +45,15 @@
 import Input from "@/components/ui/input/Input.vue";
 import ConfigOptionFrame from "@/components/settings/components/ConfigOptionFrame.vue";
 import { useSettingsStore } from "@/stores/useSettingsStore";
+import Separator from "@/components/ui/separator/Separator.vue";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectLabel,
+} from "@/components/ui/select";
 
 const settingsStore = useSettingsStore();
 </script>
