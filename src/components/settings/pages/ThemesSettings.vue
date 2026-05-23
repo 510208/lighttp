9fb42cda4266053a4cf925d3f3996a0314b91be2
SamHacker <xux510208@gmail.com>
2026-05-23 22:43:57 +0800

    ✨ feat(ThemesSettings): 新增上傳 CSS 檔案功能以自訂色彩主題並顯示當前主題資訊

--- src/components/settings/pages/ThemesSettings.vue
+++ src/components/settings/pages/ThemesSettings.vue
@@ -43,7 +43,59 @@
       label-key="settings_panel.themes.theme_file.label"
       description-key="settings_panel.themes.theme_file.description"
     >
-      <Input id="picture" type="file" accept=".css" />
+      <Input
+        id="picture"
+        type="file"
+        accept=".css"
+        @change="handleFileUpload"
+      />
+    </ConfigOptionFrame>
+    <ConfigOptionFrame
+      label-key="settings_panel.themes.current_theme.label"
+      description-key="settings_panel.themes.current_theme.description"
+    >
+      <div
+        class="bg-lh-surface-0 flex flex-1 flex-col gap-1 rounded border p-2"
+        v-if="settingsStore.colorTheme.css"
+      >
+        <p class="text-sm">
+          {{ settingsStore.colorTheme.name }}
+        </p>
+        <p class="text-muted-foreground text-sm">
+          {{ settingsStore.colorTheme.description }}
+        </p>
+        <ol class="text-muted-foreground text-sm">
+          <li>
+            {{
+              $t("settings_panel.themes.current_theme.author", {
+                author: settingsStore.colorTheme.author,
+              })
+            }}
+          </li>
+          <li>
+            {{
+              $t("settings_panel.themes.current_theme.license", {
+                license: settingsStore.colorTheme.license,
+              })
+            }}
+          </li>
+        </ol>
+      </div>
+      <p class="text-muted-foreground" v-else>
+        {{ $t("settings_panel.themes.current_theme.no_custom") }}
+      </p>
+    </ConfigOptionFrame>
+    <ConfigOptionFrame
+      label-key="settings_panel.themes.reset_theme.label"
+      description-key="settings_panel.themes.reset_theme.description"
+    >
+      <Button
+        @click="resetColorTheme()"
+        variant="destructive"
+        :disabled="!themeIsCustom"
+        ><RotateCcw />
+        {{ $t("settings_panel.themes.reset_theme.reset_button_label") }}
+      </Button>
     </ConfigOptionFrame>
   </div>
 </template>
@@ -51,7 +103,11 @@
 <script setup lang="ts">
 import Input from "@/components/ui/input/Input.vue";
 import ConfigOptionFrame from "@/components/settings/components/ConfigOptionFrame.vue";
-import { useSettingsStore } from "@/stores/useSettingsStore";
+import {
+  useSettingsStore,
+  loadColorTheme,
+  ColorTheme,
+} from "@/stores/useSettingsStore";
 import Separator from "@/components/ui/separator/Separator.vue";
 import {
   Select,
@@ -61,7 +117,49 @@ import {
   SelectValue,
   SelectLabel,
 } from "@/components/ui/select";
-import Button from "@/components/ui/button/Button.vue";
+import { Button } from "@/components/ui/button";
+import { toast } from "vue-sonner";
+import { computed } from "vue";
+import { RotateCcw } from "@lucide/vue";
 
 const settingsStore = useSettingsStore();
+const themeIsCustom = computed(() => !!settingsStore.colorTheme.css);
+
+function handleFileUpload(event: Event) {
+  const target = event.target as HTMLInputElement;
+  // 確保有選擇文件
+  if (!target.files || target.files.length === 0) {
+    return;
+  }
+
+  const file = target.files[0];
+
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    const cssContent = e.target?.result as string;
+
+    try {
+      const themeObj = loadColorTheme(cssContent);
+      settingsStore.colorTheme = themeObj;
+      toast.success("CSS 文件已成功上傳！");
+    } catch (error) {
+      toast.error("CSS 文件格式無效！");
+    }
+  };
+
+  reader.readAsText(file);
+}
+
+function resetColorTheme() {
+  const defaultTheme = {
+    name: "Mocha Theme",
+    description: "The default Catppuccin Mocha theme for LigHTTP.",
+    author: "SamHacker",
+    license: "MIT",
+
+    css: null,
+  } as ColorTheme;
+  settingsStore.colorTheme = defaultTheme;
+  toast.success("主題已重置為預設！");
+}
 </script>
