33eebdb0277ccee8b535a741f5061f9e7a593aca
SamHacker <xux510208@gmail.com>
2026-05-24 09:42:02 +0800

    ✨ feat(ConfigLoader): 新增模板以啟動 Tauri 並載入設定

--- src/components/core/ConfigLoader.vue
+++ src/components/core/ConfigLoader.vue
@@ -1,3 +1,8 @@
+<template>
+  <!-- 此組件不渲染任何內容，只負責在掛載時啟動 Tauri 並載入設定 -->
+  <div class="hidden"></div>
+</template>
+
 <script lang="ts" setup>
 import { useSettingsStore } from "@/stores/useSettingsStore";
 import { onMounted } from "vue";
