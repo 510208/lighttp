63b77ac366a292739d383569856a6240ef2f567f
SamHacker <xux510208@gmail.com>
2026-08-09 19:59:24 +0800

    ✨ feat(App): 完成自動更新邏輯以改善用戶體驗

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,20 @@
 
 <script setup lang="ts">
 import { Toaster } from "@/components/ui/sonner";
+import { onMounted } from "vue";
 import "vue-sonner/style.css";
+import { useSettingsStore } from "@/stores/useSettingsStore";
+import { checkForUpdates } from "@/services/updater";
+
+const settingsStore = useSettingsStore();
+
+onMounted(() => {
+  // #region 自動更新
+  if (settingsStore.autoUpdate) {
+    checkForUpdates();
+  }
+  // #endregion
+});
 </script>
 
 <style scoped></style>
