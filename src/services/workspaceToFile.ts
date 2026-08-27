0443dd1f641def4f654bbadbac55a41781d691ef
SamHacker <xux510208@gmail.com>
2026-08-27 12:17:10 +0800

    ✨ feat(workspace): 新增Proxy設定警告對話框以提示用戶

--- src/services/workspaceToFile.ts
+++ src/services/workspaceToFile.ts
@@ -3,9 +3,10 @@ import { useRequestStore } from "@/stores/useRequestStore";
 import { save, open } from "@tauri-apps/plugin-dialog";
 import { writeTextFile } from "@tauri-apps/plugin-fs";
 import { toast } from "vue-sonner";
+import { Dialog } from "@/services";
 
 async function saveWorkspaceToFile(): Promise<void> {
-  const data = useRequestStore().getRequestData();
+  let data = useRequestStore().getRequestData();
   const dataString = JSON.stringify(data, null, 2);
 
   const filePath = await save({
@@ -19,6 +20,34 @@ async function saveWorkspaceToFile(): Promise<void> {
   if (!filePath) {
     toast.error("選擇的儲存路徑為空");
     return;
+  }
+
+  // 檢查是否有Proxy設定，如果有，提示用戶是否要保存
+  const hasProxy = data.proxy && Object.keys(data.proxy).length > 0;
+  if (hasProxy) {
+    const userChoice = await Dialog.popDialog({
+      type: "warning",
+      title: "Proxy 設定警告",
+      description:
+        "您有一些請求設定了 Proxy。請注意，保存這些設定可能會導致安全風險。您確定要繼續保存嗎？\n\n如果您需要保存 Proxy 設定，請選擇「是」。如果您不想保存 Proxy 設定，請選擇「否」。如果您想取消存檔操作，請選擇「取消」。",
+      buttons: "yes-no-cancel",
+    });
+
+    if (userChoice === "cancel") {
+      toast.error("已取消保存工作");
+      return;
+    }
+
+    if (userChoice === "no") {
+      // 移除 Proxy 設定並設為預設值
+      data.proxy = {
+        enabled: false,
+        checkBeforeSend: false,
+        protocol: "http",
+        host: "",
+        port: 0,
+      };
+    }
   }
 
   await writeTextFile(`${filePath}`, dataString);
