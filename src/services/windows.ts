ad4df9daf44c727a302f65b48e3913bda7ea8e33
SamHacker <xux510208@gmail.com>
2026-08-27 21:59:58 +0800

    🎨 fix(windows): 調整關於視窗的寬度和高度

--- src/services/windows.ts
+++ src/services/windows.ts
@@ -4,8 +4,8 @@ function openAboutWindow() {
   const aboutWindow = new WebviewWindow("about", {
     url: "/about", // 或是您的路由路徑
     title: "關於應用程式",
-    width: 400,
-    height: 600,
+    width: 600,
+    height: 500,
     resizable: false,
     maximizable: false,
     alwaysOnTop: true, // 讓它保持在最上層
