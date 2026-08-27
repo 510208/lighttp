d23bec8bd7296e885c7ae900ecc52729f4f48384
SamHacker <xux510208@gmail.com>
2026-08-27 22:00:06 +0800

    ✨ feat(index): 新增 GitHubInfo 模組的導出

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,5 +4,6 @@ import * as FileHelpers from "./workspaceToFile";
 import * as ResponseHelpers from "./responseToFile";
 import * as Updater from "./updater";
 import * as Dialog from "./dialog";
+import * as GitHubInfo from "./ghInfo";
 
-export { Request, Window, FileHelpers, ResponseHelpers, Updater, Dialog };
+export { Request, Window, FileHelpers, ResponseHelpers, Updater, Dialog, GitHubInfo };
