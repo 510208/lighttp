e237ce78d4dfcfe715e0249c7f3a4f7fb620aedb
SamHacker <xux510208@gmail.com>
2026-08-28 12:20:06 +0800

    🩹 fix(auth): 修正 escapeHtml 函數中的引號替換錯誤

--- src/components/Home/SettingPanel/body/BodyPanel.vue
+++ src/components/Home/SettingPanel/body/BodyPanel.vue
@@ -40,7 +40,7 @@ const escapeHtml = (str: string) =>
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
-    .replace(/\"/g, "&quot;")
+    .replace(/"/g, "&quot;")
     .replace(/'/g, "&#039;");
 
 const selectedDescriptionHtml = computed(() => {
